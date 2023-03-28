177711c64934dbf2490e0fbbfc669b4ed01e6b5e
maseghepensu <45260263+maseghepensu@users.noreply.github.com>
2023-03-28 14:21:47 +0200

    Add files via upload

--- rust_x_c++_programmers.rs
+++ rust_x_c++_programmers.rs
@@ -1,9 +1,14 @@
-// esempi rust da rust x c++ programmers
-
 // riceve il parametro per valore, quindi avviene la move di una variabile v passata nella call
 // al parametro _ ; quindi nel punto dopo la call , v non e' piu' usabile (vedi (3))
 fn foo(_: Box<i32>) {
     
+}
+fn foobyr(_: &Box<i32>) {
+    
+}
+fn foomod(x: &mut Box<i32> ) {
+    // **x = 19;  // ok
+    **x = **x * 10;  // x : &mut Box<i32>, *x = mut Box<i32>, **x = i32;
 }
 
 fn main() {
@@ -35,4 +40,11 @@ fn main() {
     // rustc suggerisce di fare foo(zm.clone()):
     foo(y.clone());
     println!("dopo foo(y.clone()), y={}", y);  // dopo foo(y.clone()), y=8
+    println!("x={}",x);   // 7
+    foobyr(&x);
+    println!("x={}",x);   // ok sempre accessibile
+    
+    let mut xm = Box::new(16);   // xm deve essere mut, altrimenti non puoi passarla come &mut Box<i32>
+    foomod(&mut xm);
+    println!("xm={}",xm);   // ok sempre accessibile: xm=160
 }
